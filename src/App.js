77787dd7d8fd2c860d4903a8ce0b085d0a780bcb
Melanie <melanie.rawluk@gmail.com>
2024-03-20 20:40:10 -0600

    Add initial file/folder structure, routes. Install Material UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Dashboard from './pages/Dashboard/Dashboard';
+import MedList from './pages/MedList/MedList';
+import AddNewMed from './pages/AddNewMed/AddNewMed';
+import Notes from './pages/Notes/Notes';
+import Profile from './pages/Profile/Profile';
+
+// Med detail and Med history modals?
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/medications' element={<MedList />} />
+        <Route path='/add' element={<AddNewMed />} />
+        <Route path='/notes' element={<Notes />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
