932a3c6cd694bb94edad6a18d1595341430f6a0c
Melanie <melanie.rawluk@gmail.com>
2024-03-24 15:35:23 -0600

    Fix add new med form

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,14 +1,19 @@
 import "./Input.scss";
 
-function Input({ label, name, type, value }) {
+export default function Input({ label, name, type, value, onChange }) {
     return (
         <div className="field">
             <label htmlFor={name} className="field__label">
                 {label}
             </label>
-            <input type={type} id={name} name={name} value={value} className="field__input" />
+            <input
+                type={type}
+                id={name}
+                name={name}
+                value={value}
+                onChange={onChange}
+                className="field__input"
+            />
         </div>
     );
 }
-
-export default Input;
--- src/components/MedForm/MedForm.jsx
+++ src/components/MedForm/MedForm.jsx
@@ -1,22 +1,101 @@
 import './MedForm.scss';
 import Input from '../Input/Input';
+import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
 
-export default function MedForm({ handleOnSubmit, newMedData }) {
+export default function MedForm() {
 
 
     // if statement for frequency > adds more inputs for schedule
+    // make drop down for frequency
+
+    const base_url = process.env.REACT_APP_BASE_URL;
+    const navigate = useNavigate();
+    const { userId } = useParams();
+
+    const [medData, setMedData] = useState({
+        active: 1,
+        name: '',
+        dose: '',
+        frequency: '',
+        times: '',
+        user_id: ''
+    });
 
 
+    // Handle the form submit
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
+        // validation?
+
+        // create new obj to send data
+        const newMedObj = {
+            active: 1,
+            name: medData.name,
+            dose: medData.dose,
+            frequency: medData.frequency,
+            times: medData.times,
+            user_id: userId
+        };
+
+        try {
+            const response = await axios.post(`${base_url}/meds/add/${userId}`, newMedObj);
+            console.log(response)
+            console.log(medData)
+            navigate(`/medications/${userId}`)
+        } catch (error) {
+            console.error("Error adding medication:", error);
+        }
+        console.log(newMedObj)
+    }
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setMedData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
 
     return (
+
         <div className='med-form__container'>
-            <form className='med-form' type="submit" onClick={handleOnSubmit}>
+            <form className='med-form' onSubmit={handleSubmit}>
                 <div className='med-form__inputs'>
-                    <Input type="text" name="medication_name" label="MEDICATION NAME" />
-                    <Input type="text" name="dose" label="STRENGTH" />
-                    <Input type="text" name="frequency" label="FREQUENCY" />
-                    <Input type="text" name="times" label="SCHEDULE" />
+                    <Input
+                        type="text"
+                        htmlFor="medication_name"
+                        name="name"
+                        label="MEDICATION NAME"
+                        value={medData.name}
+                        onChange={handleInputChange}
+                    />
+                    <Input
+                        type="text"
+                        htmlFor="dose"
+                        name="dose"
+                        label="STRENGTH"
+                        value={medData.dose}
+                        onChange={handleInputChange}
+                    />
+                    <Input
+                        type="text"
+                        htmlFor="frequency"
+                        name="frequency"
+                        label="FREQUENCY"
+                        value={medData.frequency}
+                        onChange={handleInputChange}
+                    />
+                    <Input
+                        type="text"
+                        htmlFor="times"
+                        name="times"
+                        label="SCHEDULE"
+                        value={medData.times}
+                        onChange={handleInputChange}
+                    />
                 </div>
                 <button type="submit" className="med-form__button" >Done</button>
             </form>
